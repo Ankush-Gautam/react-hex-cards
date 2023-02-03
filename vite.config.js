b9951ac74bb6608fb8028692c87939d6868161fd
Ag <theankushgautam@gmail.com>
2023-02-03 10:49:31 +0545

    base address corrected

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/react-hex-card/',
+  base: '/react-hex-cards/',
   plugins: [react()],
 })
